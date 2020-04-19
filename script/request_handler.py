63a848e49364032e15206a47d10850c34693e929
Antonio Brandi <antonio.brandi@outlook.it>
2020-04-19 17:21:53 +0200

    conclusione della comunicazione tra il braccio e ros

--- script/request_handler.py
+++ script/request_handler.py
@@ -7,16 +7,18 @@ from sensor_msgs.msg import JointState
 # arduino is listening
 def publish(msg):
     pub = rospy.Publisher('servo_actuator', Float64MultiArray, queue_size=10)
-    pub.publish(msg)
+    pub.publish(data=msg)
 
 # a new message has been published on the topic /move_group/fake_controller_joint_states
 # this message is a sensor_msg/JointState message type
 # get the data and publish those on the connected arduino as joint angles
 def callback(data):
     anglesRad = data.position
-    # conversion between radiants and degees
-    anglesDeg = [angle*180/3.14 for angle in anglesRad]
-    publish(anglesDeg)
+    # conversion between radiants and percentage
+    # output of this is a number between 0 and 1
+    anglesPerc = [(angle+1.57075)/3.1415 for angle in anglesRad]
+    print "Angles %s ", anglesPerc
+    publish(anglesPerc)
     
 def listener():
     rospy.init_node('request_handler', anonymous=True)
