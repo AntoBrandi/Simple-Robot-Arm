4db5d6b0715f8d2f2246605a5388b46379c18e57
Antonio Brandi <antonio.brandi@outlook.it>
2020-04-24 13:01:33 +0200

    lettura da joint states

--- arduino/servo_control/servo_control.ino
+++ arduino/servo_control/servo_control.ino
@@ -1,37 +1,37 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <ros.h>
-#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/UInt8MultiArray.h>
 
 #define JOINT0 8
 #define JOINT1 9
 #define JOINT2 10
+#define MIN_RANGE 0
+#define MAX_RANGE 180
 
 Servo servo_joint0;
 Servo servo_joint1;
 Servo servo_joint2;
-void servo_cb( const std_msgs::Float64MultiArray& msg )
-{
-  const float min = 0;
-  const float range = 180;
-  float angle0 = msg.data[0];
-  float angle1 = msg.data[1];
-  float angle2 = msg.data[2];
-  
-  if( angle0 > 1 ) angle0 = 1;
-  if( angle0 < 0 ) angle0 = 0;
-  if( angle1 > 1 ) angle1 = 1;
-  if( angle1 < 0 ) angle1 = 0;
-  if( angle2 > 1 ) angle2 = 1;
-  if( angle2 < 0 ) angle2 = 0;
-  
-  servo_joint0.write(min + (range * angle0));
-  servo_joint1.write(min + (range * angle1));
-  servo_joint2.write(min + (range * angle2));
+
+void actuate(const std_msgs::UInt8MultiArray& msg){
+  // check that the received data are bounded correctly
+  if(msg.data[0]<MIN_RANGE) msg.data[0] = MIN_RANGE;
+  if(msg.data[1]<MIN_RANGE) msg.data[1] = MIN_RANGE;
+  if(msg.data[2]<MIN_RANGE) msg.data[2] = MIN_RANGE;
+
+  if(msg.data[0]>MAX_RANGE) msg.data[0] = MAX_RANGE;
+  if(msg.data[1]>MAX_RANGE) msg.data[1] = MAX_RANGE;
+  if(msg.data[2]>MAX_RANGE) msg.data[2] = MAX_RANGE;
+
+  // apply the received angles to the servo motors
+  servo_joint0.write((int)msg.data[0]);
+  servo_joint1.write((int)msg.data[1]);
+  servo_joint2.write((int)msg.data[2]);
 }
 
-ros::Subscriber<std_msgs::Float64MultiArray> sub("/servo_actuator", servo_cb );
 ros::NodeHandle nh;
+ros::Subscriber<std_msgs::UInt8MultiArray> sub("/servo_actuator", actuate );
+
 
 void setup()
 {
--- script/request_handler.py
+++ script/request_handler.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import JointState
+from std_msgs.msg import UInt8MultiArray
 
 # function that publishes a message on the topic ,on which the 
 # arduino is listening
 def publish(msg):
-    pub = rospy.Publisher('servo_actuator', Float64MultiArray, queue_size=10)
+    pub = rospy.Publisher('servo_actuator', UInt8MultiArray, queue_size=10)
     pub.publish(data=msg)
 
 # a new message has been published on the topic /move_group/fake_controller_joint_states
-# this message is a sensor_msg/JointState message type
+# this message is a sensor_msg/JointStatmessage type
 # get the data and publish those on the connected arduino as joint angles
 def callback(data):
     anglesRad = data.position
     # conversion between radiants and percentage
-    # output of this is a number between 0 and 1
-    anglesPerc = [(angle+1.57075)/3.1415 for angle in anglesRad]
+    # output of this is a number between 0 and 180
+    anglesPerc = [int(((angle+1.57075)*180)/3.1415) for angle in anglesRad]
     print "Angles %s ", anglesPerc
     publish(anglesPerc)
     
 def listener():
     rospy.init_node('request_handler', anonymous=True)
     # init the subscriber
-    rospy.Subscriber("/move_group/fake_controller_joint_states", JointState, callback)
+    rospy.Subscriber("/joint_states", JointState, callback)
     
 if __name__ == '__main__':
     listener()
